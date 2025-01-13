4acab078dea9c3019988d95da8fdd9c318cc6d03
minyeong <my011208@gmail.com>
2025-01-14 01:12:11 +0900

    first commit

---
+++ src/main/java/com/server/Dwenoeim/DwenoeimApplication.java
@@ -0,0 +1,13 @@
+package com.server.Dwenoeim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwenoeimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwenoeimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/Dwenoeim/DwenoeimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.Dwenoeim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwenoeimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
