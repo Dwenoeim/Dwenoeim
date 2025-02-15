9806015c8ac1765f8453c48b874d594ee5d3660c
minyeong <my011208@gmail.com>
2025-02-16 01:18:23 +0900

    feat: Swagger에 API 문서화 요소 추가
    - Swagger에 API명세
    - `@SecurityRequirement(name = "Authorization")` 적용하여 인증이 필요한 API 명시
    - Spring Security 설정 수정하여 Swagger 관련 엔드포인트 접근 허용

--- src/main/java/com/server/Dwenoeim/util/controller/AmazonS3Controller.java
+++ src/main/java/com/server/Dwenoeim/util/controller/AmazonS3Controller.java
@@ -6,8 +6,14 @@ import com.server.Dwenoeim.dto.FileUploadResponse;
 import com.server.Dwenoeim.dto.ImageResponse;
 import com.server.Dwenoeim.service.AwsS3Service;
 import io.jsonwebtoken.io.IOException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -22,9 +28,20 @@ public class AmazonS3Controller {
 
     private final AwsS3Service awsS3Service;
 
-    @PostMapping("/upload") // 사진 업로드
-    public ResponseEntity<FileUploadResponse> uploadFile(@RequestParam("file") List<MultipartFile> multipartFiles,
-                                                         @RequestParam("userId") Long userId){
+    @Operation(summary = "사진 업로드", description = "유저의 사진 업로드",
+            security = @SecurityRequirement(name = "Authorization"))
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "업로드 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<FileUploadResponse> uploadFile(
+            @Parameter(description = "사진 파일", required = true)
+            @RequestPart("file") List<MultipartFile> multipartFiles,
+
+            @Parameter(description = "유저 아이디", required = true, example = "123")
+            @RequestPart("userId") Long userId){
         if (multipartFiles == null || multipartFiles.isEmpty()) {
             return ResponseEntity.badRequest().body(new FileUploadResponse("파일이 없습니다.", null));
         }
@@ -37,19 +54,41 @@ public class AmazonS3Controller {
 
 
     @DeleteMapping("/delete")  // 사진 삭제
+    @Operation(summary="사진 삭제", description = "파일명으로 사진 하나를 삭제합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "업로드 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
     public ResponseEntity<String> deleteFile(@RequestParam("fileName") String fileName){
         awsS3Service.deleteFile(fileName);
         return ResponseEntity.ok(fileName);
     }
 
+
+    @Operation(summary="사진 url 한개 검색", description = "파일명으로 사진 하나를 검색")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "업로드 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
     @GetMapping("/view") //파일 이름으로 사진url검색
     public ResponseEntity<FileResponse> getFileUrl(@RequestParam String fileName) {
         String fileUrl = awsS3Service.getFileUrl(fileName);
         return ResponseEntity.status(HttpStatus.OK).body(new FileResponse(fileUrl));
     }
 
+    @Operation(summary = "사진 리스트(앨범) 조회", description = "유저id로 사진url 리스트 조회",
+            security = @SecurityRequirement(name = "Authorization"))
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "업로드 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
     @GetMapping("/user") //유저id로 사진url 리스트 검색
-    public ResponseEntity<ImageResponse> getUserImages(@RequestParam("userId") Long userId) {
+    public ResponseEntity<ImageResponse> getUserImages(
+            @Parameter(description = "유저 아이디", required = true)
+            @RequestParam("userId") Long userId) {
         List<String> userImages = awsS3Service.getUserImages(userId);
         return ResponseEntity.status(HttpStatus.OK).body(new ImageResponse(userImages));
     }
